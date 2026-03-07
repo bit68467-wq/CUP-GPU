b1b2c2705c502319571337ca08fca12dbdb193fd
bit68467-wq <bit68467@gmail.com>
2026-03-07 15:22:35 +0100

    Delete api.js

--- api.js
+++
@@ -1,7 +0,0 @@
-import { MockBackend } from './mockBackend.js';
-import { RealBackend } from './realBackend.js';
-
-const API_BASE = window.CUP9_API_BASE || null;
-
-/* tombstone: large mock/real backend implementations moved to mockBackend.js and realBackend.js */
-export const API = API_BASE ? RealBackend : MockBackend;
